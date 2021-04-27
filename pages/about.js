3c8051881dbd8186c1c9b413a49cb2342470a6a4
Cassiano Montanari <cassianomontanari@gmail.com>
2021-04-28 00:40:58 +0200

    Added homepage to package.json

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,24 @@
 export default function AboutPage() {
   return (
     <div>
-      <p>About me</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse ex
+        voluptatum, doloribus itaque aliquid iure quo sit qui dolor blanditiis
+        hic obcaecati fuga consequatur, recusandae vel excepturi tempora alias
+        libero?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse ex
+        voluptatum, doloribus itaque aliquid iure quo sit qui dolor blanditiis
+        hic obcaecati fuga consequatur, recusandae vel excepturi tempora alias
+        libero?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse ex
+        voluptatum, doloribus itaque aliquid iure quo sit qui dolor blanditiis
+        hic obcaecati fuga consequatur, recusandae vel excepturi tempora alias
+        libero?
+      </p>
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,7 @@
 export default function IndexPage() {
   return (
-    // <div>
-    //   <p>Hi, my name is Cassiano Montanari</p>
-    // </div>
     <div>
-      <p>Hello</p>
+      <p>Hi, my name is Cassiano Montanari</p>
     </div>
   );
 }
